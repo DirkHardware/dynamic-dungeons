94dd7a09b2217490c890180453070585f7ae63e0
DirkHardware <email.the.anderson@gmail.com>
2020-09-15 20:52:40 -0400

    dynamic class assignment testing and psudeocode

--- grid.py
+++ grid.py
@@ -9,6 +9,7 @@ class Grid(object):
         self.__setY = height
         # We don't need entrance dirs because we can just use entrance
         # dirs to set our first anchors instead.
+        self.valid_structures = []
         self.anchors = []
         self.squares = [[]]
         for columns in range(0, self.__setX):
@@ -21,6 +22,56 @@ class Grid(object):
             self.squares.append(row)
 
 
+class Duck(object):
+
+    def speak(self):
+        print("quack")
+
+
+class Dog(object):
+
+    def speak(self):
+        print("woof")
+
+
+class Frog(object):
+
+    def speak(self):
+        print('ribbit')
+
+
+
+
+
+# Grid starts by randomly generating anchors
+# Structure classes a distinct signature shape expressable as a matrix that the grid class
+# can read an apply to itself. These signatures must be rotatable by compass direction.
+# Does the grid need to know the direction of all of its anchor points?
+# What does the grid do apart from creating and exporting itself, along with
+# keeping track of anchor points.
+# Is it grids job to keep track of what structures it should or shouldn't be building?
+
+# Psuedocode: STRUCTURES
+"""
+    args(self, grid.squares, anchors, compass_axis[i.e. North-South, West-East)
+    attributes:
+    layout: a matrix that the subclass will use to operate on the grid and
+    build itself out from the anchors provided. 
+    area: the amount of space taken up by the matrix
+    volume: the total amount of movable squares used in the structure. This might be 
+    used to lend weight to certain structures. Perhaps a grid sector needs at least one room of a 
+    certain volume so it's not just constantly building halls?
+    
+    returns a new set of anchors for the grid to operate on. 
+    
+    structure subclasses 
+"""
+
+
+
 if __name__ == '__main__':
-    grid = Grid(16, 16)
-    print(grid.squares)
+    potential_classes = [Duck(), Dog(), Frog()]
+    pet = potential_classes[random.randint(0, 2)]
+    pet.speak()
+    # grid = Grid(16, 16)
+    # print(grid.squares)
