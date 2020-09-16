a22db02370b11af9f27ad0d04054027a8ad5ccc3
DirkHardware <email.the.anderson@gmail.com>
2020-09-16 14:37:39 -0400

    rudimentary generation from classes!

--- grid.py
+++ grid.py
@@ -31,6 +31,7 @@ class Grid(object):
                 row.append(0)
             self.squares.append(row)
 
+    # Do we actually need this method?
     def offset(self):
         offset = 0
         offset_found = False
@@ -38,17 +39,33 @@ class Grid(object):
             for column in self.current_structure.layout[0]:
                 if column == structures.x:
                     offset -= 1
+                    # print(offset)
                 else:
                     offset_found = True
+                    # print(offset)
         self.anchor[1] += offset
+        # print(self.anchor)
+        # print(offset)
         return self.anchor
 
     def build(self):
         # print(self.current_structure)
-        self.anchor = self.offset()
-        for column in self.current_structure.layout:
-            print(column)
-
+        # THIS SHOULD BE USED FOR NORTH TO SOUTH STRUCTURES ONLY
+        # self.anchor = self.offset()
+        current_y = self.anchor[0]
+        current_x = self.anchor[1]
+        while current_y < len(self.current_structure.layout[0]) + 1:
+            for square in self.current_structure.layout[current_y]:
+                if square is not structures.x:
+                    grid.squares[current_y][current_x] = square
+                    current_x += 1
+                else:
+                    current_x += 1
+                    pass
+            current_y += 1
+            current_x = self.anchor[1]
+        print(self.squares)
+        # print("Current X:{0}, Current Y: {1}".format(current_x, current_y))
 
 
 # Grid starts by randomly generating anchor
@@ -59,12 +76,43 @@ class Grid(object):
 # keeping track of anchor points.
 # Is it grids job to keep track of what structures it should or shouldn't be building?
 
+def emptyGrid(intDim):
+    # myPen.begin_fill()
+    # 0 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 90 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 180 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 270 deg.
+    myPen.forward(intDim)
+    # myPen.end_fill()
+    myPen.setheading(0)
+
+
+def fillGrid(intDim):
+    myPen.begin_fill()
+    # 0 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 90 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 180 deg.
+    myPen.forward(intDim)
+    myPen.left(90)
+    # 270 deg.
+    myPen.forward(intDim)
+    myPen.end_fill()
+    myPen.setheading(0)
 
 if __name__ == '__main__':
     grid = Grid(16, 16)
     grid.offset()
     grid.build()
-    # grid.build()
     # hallway = structures.ShortHallway(0)
     # hallway.check_attributes()
     # hallway.blueprint()
@@ -74,3 +122,37 @@ if __name__ == '__main__':
     # pet.speak()
     # grid = Grid(16, 16)
     # print(grid.squares)
+    myPen = turtle.Turtle()
+    # myPen.tracer(0)
+    myPen.speed(0)
+    myPen.color("#000000")
+
+    boxSize = 10
+    # Position myPen in top left area of the screen
+    myPen.penup()
+    # Below var was originally -100 I think.
+    myPen.forward(-(len(grid.squares)*3.5))
+    myPen.setheading(90)
+    myPen.forward(100)
+    myPen.setheading(0)
+
+    for i in range(0, len(grid.squares)):
+        for j in range(0, len(grid.squares[i])):
+            if grid.squares[i][j] == 0:
+                emptyGrid(boxSize)
+                # print("Printing grid row {0}".format(i))
+            else:
+                fillGrid(boxSize)
+            myPen.penup()
+            myPen.forward(boxSize)
+            myPen.pendown()
+        myPen.setheading(270)
+        myPen.penup()
+        myPen.forward(boxSize)
+        myPen.setheading(180)
+        myPen.forward(boxSize * len(grid.squares[i]))
+        myPen.setheading(0)
+        myPen.pendown()
+
+    myPen.getscreen().update()
+    turtle.done()
